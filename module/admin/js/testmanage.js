ec44f5136cb31c71eaed0f144378fc7a2a5ab412
Jian <dreamhufly@163.com>
2016-04-06 17:32:00 +0800

    lose fix

--- module/admin/js/testmanage.js
+++ module/admin/js/testmanage.js
@@ -1,11 +1,33 @@
-return:1, Time:20160404102530
-2016-4-4 10:25:30 361  KCs3::IsNeedUpdate return 1
-2016-4-4 10:25:30 361  KCs3::IsNeedUpdate return 1
-2016-4-4 10:25:30 361  KCs3::IsNeedUpdateFast return 1
-2016-4-4 10:25:30 361  KDubaUpdate::CheckDiskSize diskSpace:5320 MB
-2016-4-4 10:25:30 361  KUpdateInst::CreateChannels Add Update Channel, name:kwifi, checkfile:
-2016-4-4 10:25:30 361  KUpdateInst::CreateChannels return:0
-2016-4-4 10:25:30 361  KUpdateInst::Create return:0
-2016-4-4 10:25:30 361  KUpdateInst::DownloadIndex ...
-2016-4-4 10:25:30 361  KHelper::DownloadFileAndDecompressCrcDat url:http://wifi.www.duba.net/kwifi/bin/1888/59e30c94.dat file:C:\Program Files (x86)\kingsoft\kwifi\fastupdate\temp\kwifi.txt
-2016-4-4 10:25:30 486  KHelper::DownloadFileAndDecompress url:http://wifi.www.duba.net/kwifi/bin/1888/kwifi/bef093a
+$(document).ready(function () {
+	$('.js-modify').click(function () {
+		var $id = $(this).parent().parent().children().first().html();
+		var url = 'testmodify.php?id=' + $id;
+		$.ajax({
+			type: 'GET',
+			url: url,
+			success: function() {
+				console.log("文章修改");
+				window.open(url,'_self');
+				}
+			});
+		});
+		
+	$('.js-delete').click(function () {
+		var $deleteid = $(this).parent().parent().children().first().html();
+		var deleteurl = 'handle/testdelete.handle.php?id=' + $deleteid;
+		$.ajax({
+			type: 'GET',
+			url: deleteurl,
+			dataType:'json',
+			success: function (data) {
+				if (data.success) {
+						window.location.href = 'testmanage.php';
+					}
+				else {
+						alert(data.message);
+						window.location.href = 'testmanage.php';
+					}
+				}
+			});
+		});
+});
